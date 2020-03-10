59f7e576b945a67648504338c4542bf631dff6a6
Koji <koji.52884da@gmail.com>
2020-03-09 22:02:10 -0700

    I created a file and added in the insert.sql

---
+++ insert.sql
@@ -0,0 +1,6 @@
+-- insert id, name, price, character_name, category, purchased_at from purchases.
+
+INSERT INTO purchases(name,price,character_name,category,purchased_at)
+VALUES("スマホケース",1500,"にんじゃわんこ","雑費",2017-07-01); 
+
+-- select * from prog8sql;
