67a87de327bcdf8c81ef13be2cc41cd71896a41a
Koji <koji.52884da@gmail.com>
2020-03-10 23:42:57 -0700

    I added some coding in insert.sql

--- insert.sql
+++ insert.sql
@@ -1,5 +1,35 @@
 -- insert id, name, price, character_name, category, purchased_at from purchases.
 
 INSERT INTO purchases(name,price,character_name,category,purchased_at)
-VALUES("スマホケース",1500,"にんじゃわんこ","雑費",2017-07-01); 
+VALUES("Smartphone case",1500,"ninjadog","miscellaneous expenses","2017-07-01"),
+("bento",500,"sheepmaster","food expenses","2017-07-01"),
+("one piece",3000,"torizukin","miscellaneous expenses","2017-07-01"),
+("reference book",2000,"ninnjadog","miscellaneous expenses","2017-07-01"),
+("train fare",450,"sheepmaster","transportation expenses","2017-07-01"),
+("pudding",150,"ninjadog","food expenses","2017-07-02"),
+("soccer ball",2500,"babydog","miscellaneous expenses","2017-07-02"),
+("calculator",700,"sheepmaster","miscellaneous expenses","2017-07-04"),
+("bento",1000,"ninjadog","food expenses","2017-07-04"),
+("pudding parfait",500,"ninjadog","food expenses","2017-07-04"),
+("taxi fare",1200,"torizukin","transportation expenses","2017-07-09"),
+("cotton candy",100,"babydog","food expenses","2017-07-10"),
+("glasses",	5000,"sheepmaster","miscellaneous expenses","2017-07-10"),
+("baked pudding",200,"ninjadog","food expenses","2017-07-10"),
+("handbag",	8000,"torizukin","miscellaneous expenses","2017-07-15"),
+("Soy milk cookies",300,"torizukin","food expenses","2017-07-15"),
+("beef bowl",600,"ninjadog","food expenses","2017-07-15"),
+("grilled fish set meal",550,"sheepmaster","food expenses","2017-07-15"),
+("note",150,"babydog","miscellaneous expenses","2017-07-21"),
+("pencil",100,"babydog","miscellaneous expenses","2017-07-24"),
+("cheek",800,"torizukin","miscellaneous expenses","2017-07-24"),
+("parking fee",200,"ninjadog","transportation expenses","2017-07-28"),
+("butter",240,"sheepmaster","food expenses","2017-07-28"),
+("potatoes",50,"sheepmaster","food expenses","2017-07-28"),
+("milk pudding",100,"ninjadog","food expenses","2017-07-28"),
+("bus fare",230,"sheepmaster","transportation expenses","2017-08-02"),
+("soft pancakes",750,"torizukin","food expenses","2017-08-02"),
+("baked pudding daifuku",140,"ninjadog","food expenses","2017-08-03"),
+("electronic work kit",	5400,"ninjadog","miscellaneous expenses","2017-08-07"),
+("softener",600,"torizukin","miscellaneous expenses","2017-08-07");
+
 
